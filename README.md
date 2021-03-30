0512910cba762d68c538c22637c2db158ef29993
ManarAbdelkarim <manarbdelkarim434@gmail.com>
2021-03-30 23:53:35 +0300

    adding sql to location

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # city_explorer_api
+
+Number and name of feature: ______Repository Set Up__________
+
+Estimate of time needed to complete: __2 hours__
+
+Start time: __3pm__
+
+Finish time: __4pm__
+
+Actual time needed to complete: _1hour_
+
+Number and name of feature: ________ Locations_________
+
+Estimate of time needed to complete: __2hours__
+
+Start time: __4pm__
+
+Finish time: __5:30__
+
+Actual time needed to complete: __1hour and half__
+
+Number and name of feature: ______Errors____________
+
+Estimate of time needed to complete: __1hour__
+
+Start time: _5:30__
+
+Finish time: _8:30__
+
+Actual time needed to complete: __3 hours__
--- server.js
+++ server.js
@@ -1,61 +1,139 @@
 'use strict';
 
-// Load Environment Variables from the .env file
-require('dotenv').config();
-
-// Application Dependencies
+// Dependencies
 const express = require('express');
 const cors = require('cors');
-
-// Application Setup
-// const PORT = process.env.PORT;
+require('dotenv').config();
+const superAgent = require('superagent');
+// const Joi = require('joi');
+//Application Setup
+const PORT = process.env.PORT || 3030;
 const app = express();
 app.use(cors());
+const db_url = process.env.DATABASE_URL;
+// DataBase connection
+const pg  = require('pg');
+const client = new pg.Client(db_url);
 
-// routes
-app.get('/location', handelLocationRequest);
-app.get('/restaurants', handelRestaurantRequest);
-const PORT = process.env.PORT || 3000;
+let city;
+app.get('/location', locationRoute);
+function locationRoute(req, res) {
+  // const locData = require('./data/location.json');
+  city = req.query.city;
+  // let url = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
+  const url = 'https://eu1.locationiq.com/v1/search.php';
+  const query = {
+    key : process.env.GEO_CODE_API_KEY,
+    lat : req.query.latitude,
+    lon: req.query.longitude,
+    city: city,
+    format: 'json'
+  }
+  if (!city) {
+    res.status(500).send('Status 500 : sorry , something went wrong');
+  }
+  const sqlQuery = `SELECT * FROM locations WHERE search_query = '${city}';`;
+  client.query(sqlQuery).then(data => {
+    console.log(data);
+    if (data.rows.length === 0) {
+      superAgent.get(url).query(query).then(location => {
+        const locObj = new Location(city, location.body[0]);
+        const insertInto = `INSERT INTO locations
+      (search_query, formatted_query, latitude, longitude)
+      VALUES ($1,$2,$3,$4);`;
+        let values = [city, locObj.formatted_query, locObj.latitude, locObj.longitude];
 
-
-
-function handelLocationRequest(req, res) {
-
-  const searchQuery = req.query;
-  console.log(searchQuery);
-
-  const locationsRawData = require('./public/starter-code/data/location.json');
-  const location = new Location(locationsRawData[0])
-  res.send(location);
-}
-function handelRestaurantRequest(req, res) {
-  const restaurantsRawData = require('./data/restaurants.json');
-  const restaurantsData = [];
-
-  restaurantsRawData.nearby_restaurants.forEach(restaurant => {
-    restaurantsData.push(new Restaurant(restaurant));
+        client.query(insertInto, values)
+          .then(() => {
+            res.status(200).json(locObj);
+          })
+      }).catch((error) => {
+        console.error('ERROR',error);
+        req.status(500).send('no Location ya boy');
+      });
+    }
+    else if (data.rows[0].search_query === city) {
+    // get data from DB
+      const newDb = new Location(data.rows[0].search_query, data.rows[0]);
+      res.send(newDb);
+    }
+  }).catch((error) => {
+    console.error('ERROR',error);
+    req.status(500).send('no weather ya boy');
   });
 
-  res.send(restaurantsData);
-
 }
 
-// constructors
+app.get('/weather', weatherRoute);
+function weatherRoute(req, res) {
+  // let city = req.query.search_query;
+  let key = process.env.WEATHER_CODE_API_KEY;
+  let url = 'https://api.weatherbit.io/v2.0/forecast/daily';
+  const query = {
+    city :city,
+    key :key
+  }
 
-function Location(data) {
+  superAgent.get(url).query(query)
+    .then(weather => {
+      const weatherArr = weather.body.data.map(val => new Weather(val));
+      res.send(weatherArr);
+      console.log(weatherArr);
+    })
+    .catch((error) => {
+      console.error('ERROR',error);
+      req.status(500).send('no weather ya boy');
+    })
+}
+
+
+app.get('/parks', parksRoute);
+function parksRoute(req, res) {
+  // console.log(req.query);
+  // const code = req.query.latitude + ',' + req.query.longitude;
+  const key = process.env.PARK_CODE_API_KEY;
+  const url = 'https://developer.nps.gov/api/v1/parks';
+  const query = {
+    q:city,
+    parkCode: req.query.parkCode,
+    api_key:key,
+    limit :10
+  }
+  superAgent.get(url).query(query).then(parkData => {
+    // console.log(parkData.body.data[0].entranceFees[0].cost);
+    let parkArr = parkData.body.data.map(val => new Park(val));
+    res.send(parkArr);
+  }).catch((error) => {
+    console.error('ERROR',error);
+    req.status(500).send('no parks ya boy');
+  })
+}
+
+
+
+
+function Location(city, data) {
+  this.search_query = city;
   this.formatted_query = data.display_name;
   this.latitude = data.lat;
   this.longitude = data.lon;
 }
 
-function Restaurant(data) {
-  this.restaurant = data.restaurant.name;
-  this.cuisines = data.restaurant.cuisines;
-  this.locality = data.restaurant.location.locality;
+function Weather(data) {
+  this.forecast = data.weather.description;
+  this.time = new Date(data.valid_date).toString().slice(0, 15);
 }
 
-app.use('*', (req, res) => {
-  res.send('all good nothing to see here!');
-});
+function Park(data) {
+  this.name = data.name;
+  this.address = `"${data.addresses[0].line1}" "${data.addresses[0].city}" "${data.addresses[0].stateCode}" "${data.addresses[0].postalCode}"`;
+  this.fee =data.fees[0] || '5.00';
+  this.description = data.description;
+  this.url = data.url;
+}
 
-app.listen(PORT, () => console.log(`Listening to Port ${PORT}`));
+
+client.connect().then(() =>{
+  // console.log('connect to database ' , client.connectionParameter.database)
+  app.listen(PORT, () => console.log(`Listening to Port ${PORT}`));
+});
